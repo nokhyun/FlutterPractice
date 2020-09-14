c64bc61ff0c1b67663ce8b6b5a346cbe590b6693
nokhyun90 <nokhyun90@gmail.com>
2020-09-14 17:38:59 +0900

    assets, onPressed 추가

--- lib/main.dart
+++ lib/main.dart
@@ -8,112 +8,125 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Nokhyun Flutter Demo',
       theme: ThemeData(primarySwatch: Colors.blue),
-      home: MyHomePage(title: 'Nokhyun Flutter Tutorial Page'),
+      home: MyApp2(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-  final String title;
-
+class MyApp2 extends StatefulWidget {
   @override
-  _MyHomePageState createState() => _MyHomePageState();
+  _MyApp2State createState() => _MyApp2State();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  var titleSection = Row(
-    mainAxisAlignment: MainAxisAlignment.center,
-    children: <Widget>[
-      Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: <Widget>[
-          Text(
-            'Oeschinen Lake Comapground',
-            style: TextStyle(fontWeight: FontWeight.bold, fontSize: 16),
-          ),
-          Text('Kandresteg, Switzerland',
-              style: TextStyle(color: Colors.grey, fontSize: 16)),
-        ],
-      ),
-      Padding(padding: EdgeInsets.all(8.0)),
-      Icon(
-        Icons.star,
-        size: 35,
-        color: Colors.deepOrange,
-      ),
-      Text(
-        '41',
-        style: TextStyle(fontSize: 22),
-      ),
-    ],
-  );
+class _MyApp2State extends State<MyApp2> {
+  bool _isStared = false;
+  int _count = 41;
 
-  var buttonSection = Row(
-    mainAxisAlignment: MainAxisAlignment.center,
-    children: <Widget>[
-      Column(
-        children: [
-          Icon(
-            Icons.call,
-            size: 40,
-            color: Colors.lightBlue,
-          ),
-          Text(
-            'Call',
-            style: TextStyle(color: Colors.lightBlue),
-          )
-        ],
-      ),
-      Padding(padding: EdgeInsets.all(40.0)),
-      Column(
-        children: [
-          Icon(Icons.near_me, size: 40, color: Colors.lightBlue),
-          Text(
-            'ROUTE',
-            style: TextStyle(color: Colors.lightBlue),
-          )
-        ],
-      ),
-      Padding(padding: EdgeInsets.all(40.0)),
-      Column(
-        children: [
-          Icon(Icons.share, size: 40, color: Colors.lightBlue),
-          Text('SHARE', style: TextStyle(color: Colors.lightBlue))
-        ],
-      )
-    ],
-  );
-  
+  Row getTitleSection(){
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: <Widget>[
+        Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            Text(
+              'Oeschinen Lake Comapground',
+              style: TextStyle(fontWeight: FontWeight.bold, fontSize: 16),
+            ),
+            Text('Kandresteg, Switzerland',
+                style: TextStyle(color: Colors.grey, fontSize: 16)),
+          ],
+        ),
+        Padding(padding: EdgeInsets.all(8.0)),
+        IconButton(
+          icon: (_isStared? Icon(Icons.star) : Icon(Icons.star_border)),
+          color: Colors.deepOrange,
+          onPressed: () => _pressedStar(),
+        ),
+        Text(
+          '$_count',
+          style: TextStyle(fontSize: 22),
+        ),
+      ],
+    );
+  }
 
+  Row getButtonSection(){
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: <Widget>[
+        Column(
+          children: [
+            Icon(
+              Icons.call,
+              size: 40,
+              color: Colors.lightBlue,
+            ),
+            Text(
+              'Call',
+              style: TextStyle(color: Colors.lightBlue),
+            )
+          ],
+        ),
+        Padding(padding: EdgeInsets.all(40.0)),
+        Column(
+          children: [
+            Icon(Icons.near_me, size: 40, color: Colors.lightBlue),
+            Text(
+              'ROUTE',
+              style: TextStyle(color: Colors.lightBlue),
+            )
+          ],
+        ),
+        Padding(padding: EdgeInsets.all(40.0)),
+        Column(
+          children: [
+            Icon(Icons.share, size: 40, color: Colors.lightBlue),
+            Text('SHARE', style: TextStyle(color: Colors.lightBlue))
+          ],
+        )
+      ],
+    );
+  }
 
-  var textSection = Container(
-      child: Text('Lake......', style: TextStyle(fontSize: 15)),
-      padding:EdgeInsets.all(40.0)
-  );
+  Container getTextSection(){
+    return Container(
+        child: Text('Lake......', style: TextStyle(fontSize: 15)),
+        padding: EdgeInsets.all(40.0)
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Column(
         children: <Widget>[
-          Image.network(
-            '',
+          Image.asset(
+            'assets/sample/lake.jpg',
             height: 240,
             width: 600,
             fit: BoxFit.cover,
           ),
           Padding(padding: EdgeInsets.all(12.0)),
-          titleSection,
+          getTitleSection(),
           Padding(padding: EdgeInsets.all(12.0)),
-          buttonSection,
-          textSection
+          getButtonSection(),
+          getTextSection()
         ],
       ),
     );
   }
 
-  Text _getText(String str){
-    return Text(str, style: TextStyle(color: Colors.lightBlue));
+  void _pressedStar() {
+    setState(() {
+      _isStared = !_isStared;
+
+      if(_isStared){
+        _count++;
+      }else{
+        _count--;
+      }
+    });
   }
+
 }
